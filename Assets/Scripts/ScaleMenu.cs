e5981faf4651d255b4434ff6aeebe92c94369374
WaterDazed <waterhou1221@gmail.com>
2024-07-19 16:52:18 +0800

    修改问卷

--- Assets/Scripts/ScaleMenu.cs
+++ Assets/Scripts/ScaleMenu.cs
@@ -12,19 +12,17 @@ public class ScaleMenu : MonoBehaviour
     private Timer timer;
 
     private string fileName, path;
-    public string[] questionList = new string[11]
+    public string[] questionList = new string[9]
     {
         " ",
-        "在白板上写画和操作都非常流畅、舒适与自然",
+        "我与我的同伴交互时的体验非常好",
         "我能感受到我的同伴与我同在，我的同伴也能感受到我与他同在",
-        "我能轻易地掌握该白板的使用方法",
         "我感觉有点孤单，我的同伴也觉得有点孤单",
-        "这次绘画任务的完成效率非常高",
+        "这次绘画任务的完成得非常迅速",
         "我能轻松地理解我的同伴，我的同伴也能轻松理解我",
-        "与同伴的绘画协作过程十分顺利",
         "我一直都关注着我的同伴，我的同伴也一直关注着我",
         "我和我的同伴难以相互理解",
-        "我和我的同伴没有一直关注着对方"
+        "我和我的同伴没有一直相互关注",
     };
     private int questionPos;
     // Start is called before the first frame update
